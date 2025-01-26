35308ccd008f2f0ef5e7b53b506b5fb17e4bee54
AndreeMendozavk <n00312706@upn.pe>
2025-01-25 21:49:30 -0500

    Add files via upload

---
+++ Semana Proyecto 1 Cola/Semana Proyecto 1 Cola/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Semana_Proyecto_1_Cola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEncolar = new System.Windows.Forms.Button();
+            this.btnDesencolar = new System.Windows.Forms.Button();
+            this.btnVistazo = new System.Windows.Forms.Button();
+            this.btnElementos = new System.Windows.Forms.Button();
+            this.listBoxNombres = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxNombres = new System.Windows.Forms.TextBox();
+            this.btnDestruir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cola";
+            // 
+            // btnEncolar
+            // 
+            this.btnEncolar.Location = new System.Drawing.Point(43, 191);
+            this.btnEncolar.Name = "btnEncolar";
+            this.btnEncolar.Size = new System.Drawing.Size(98, 23);
+            this.btnEncolar.TabIndex = 1;
+            this.btnEncolar.Text = "Encolar";
+            this.btnEncolar.UseVisualStyleBackColor = true;
+            this.btnEncolar.Click += new System.EventHandler(this.btnEncolar_Click);
+            // 
+            // btnDesencolar
+            // 
+            this.btnDesencolar.Location = new System.Drawing.Point(43, 260);
+            this.btnDesencolar.Name = "btnDesencolar";
+            this.btnDesencolar.Size = new System.Drawing.Size(98, 23);
+            this.btnDesencolar.TabIndex = 2;
+            this.btnDesencolar.Text = "Desencolar";
+            this.btnDesencolar.UseVisualStyleBackColor = true;
+            this.btnDesencolar.Click += new System.EventHandler(this.btnDesencolar_Click);
+            // 
+            // btnVistazo
+            // 
+            this.btnVistazo.Location = new System.Drawing.Point(43, 321);
+            this.btnVistazo.Name = "btnVistazo";
+            this.btnVistazo.Size = new System.Drawing.Size(98, 23);
+            this.btnVistazo.TabIndex = 3;
+            this.btnVistazo.Text = "Vistazo";
+            this.btnVistazo.UseVisualStyleBackColor = true;
+            this.btnVistazo.Click += new System.EventHandler(this.btnVistazo_Click);
+            // 
+            // btnElementos
+            // 
+            this.btnElementos.Location = new System.Drawing.Point(43, 378);
+            this.btnElementos.Name = "btnElementos";
+            this.btnElementos.Size = new System.Drawing.Size(98, 45);
+            this.btnElementos.TabIndex = 4;
+            this.btnElementos.Text = "Cantidad De Elementos";
+            this.btnElementos.UseVisualStyleBackColor = true;
+            this.btnElementos.Click += new System.EventHandler(this.btnElementos_Click);
+            // 
+            // listBoxNombres
+            // 
+            this.listBoxNombres.FormattingEnabled = true;
+            this.listBoxNombres.ItemHeight = 16;
+            this.listBoxNombres.Location = new System.Drawing.Point(238, 191);
+            this.listBoxNombres.Name = "listBoxNombres";
+            this.listBoxNombres.Size = new System.Drawing.Size(188, 308);
+            this.listBoxNombres.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ingresar Nombre";
+            // 
+            // txtBoxNombres
+            // 
+            this.txtBoxNombres.Location = new System.Drawing.Point(278, 109);
+            this.txtBoxNombres.Name = "txtBoxNombres";
+            this.txtBoxNombres.Size = new System.Drawing.Size(100, 22);
+            this.txtBoxNombres.TabIndex = 7;
+            // 
+            // btnDestruir
+            // 
+            this.btnDestruir.Location = new System.Drawing.Point(43, 455);
+            this.btnDestruir.Name = "btnDestruir";
+            this.btnDestruir.Size = new System.Drawing.Size(98, 23);
+            this.btnDestruir.TabIndex = 8;
+            this.btnDestruir.Text = "Destruir";
+            this.btnDestruir.UseVisualStyleBackColor = true;
+            this.btnDestruir.Click += new System.EventHandler(this.btnDestruir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 561);
+            this.Controls.Add(this.btnDestruir);
+            this.Controls.Add(this.txtBoxNombres);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxNombres);
+            this.Controls.Add(this.btnElementos);
+            this.Controls.Add(this.btnVistazo);
+            this.Controls.Add(this.btnDesencolar);
+            this.Controls.Add(this.btnEncolar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEncolar;
+        private System.Windows.Forms.Button btnDesencolar;
+        private System.Windows.Forms.Button btnVistazo;
+        private System.Windows.Forms.Button btnElementos;
+        private System.Windows.Forms.ListBox listBoxNombres;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxNombres;
+        private System.Windows.Forms.Button btnDestruir;
+    }
+}
+
---
+++ Semana Proyecto 1 Cola/Semana Proyecto 1 Cola/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semana_Proyecto_1_Cola
+{
+    public partial class Form1 : Form
+    {
+        Cola c = new Cola();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncolar_Click(object sender, EventArgs e)
+        {
+
+            if (c.validarTexto(txtBoxNombres.Text))
+            {
+                c.Enqueue(txtBoxNombres.Text.Trim());
+                listBoxNombres.Items.Add(txtBoxNombres.Text);
+                listBoxNombres.Items.Clear();
+                c.Mostrar(listBoxNombres);
+            }
+            else
+            {
+                MessageBox.Show("DATO NO VALIDO");
+            }
+            txtBoxNombres.Clear();
+
+        }
+
+        private void btnDesencolar_Click(object sender, EventArgs e)
+        {
+            if(c.Dequeue() != null)
+            {
+                MessageBox.Show("Desapilado correctamente ");
+                listBoxNombres.Items.Clear();
+                c.Mostrar(listBoxNombres);
+            }
+            else
+            {
+                MessageBox.Show("LA COLA ESTA VACIA");
+            }
+        }
+
+        private void btnVistazo_Click(object sender, EventArgs e)
+        {
+           string prim = c.Peek();
+            if (prim != null)
+            {
+                MessageBox.Show("Primero " + prim);
+
+            }
+            else
+            {
+                MessageBox.Show("LA COLA ESTA VACIA");
+            }
+        }
+
+        private void btnElementos_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" #Elementos: " + c.Count());
+        }
+
+        private void btnDestruir_Click(object sender, EventArgs e)
+        {
+            c.Clear();
+            listBoxNombres.Items.Clear();
+
+        }
+    }
+}
